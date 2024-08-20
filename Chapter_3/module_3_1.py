9006667f848f4847f57e6b403a7e2d63cb2bcecd
Dmitry Morozov <9984398@gmail.com>
2024-08-20 18:05:57 +0300

    --> module_3_1.py: Completed -> Tests: None

--- Chapter_3/module_3_1.py
+++ Chapter_3/module_3_1.py
@@ -1,19 +1,10 @@
 """
 Задача "Счётчик вызовов":
-Порой необходимо отслеживать, сколько раз вызывалась та или иная функция. К сожалению, в Python не предусмотрен подсчёт вызовов автоматически.
-Давайте реализуем данную фишку самостоятельно!
 
 Вам необходимо написать 3 функции:
 Функция count_calls подсчитывающая вызовы остальных функций.
 Функция string_info принимает аргумент - строку и возвращает кортеж из: длины этой строки, строку в верхнем регистре, строку в нижнем регистре.
 Функция is_contains принимает два аргумента: строку и список, и возвращает True, если строка находится в этом списке, False - если отсутствует. Регистром строки при проверке пренебречь: UrbaN ~ URBAN.
-Пункты задачи:
-Создать переменную calls = 0 вне функций.
-Создать функцию count_calls и изменять в ней значение переменной calls. Эта функция должна вызываться в остальных двух функциях.
-Создать функцию string_info с параметром string и реализовать логику работы по описанию.
-Создать функцию is_contains с двумя параметрами string и list_to_search, реализовать логику работы по описанию.
-Вызвать соответствующие функции string_info и is_contains произвольное кол-во раз с произвольными данными.
-Вывести значение переменной calls на экран(в консоль).
 
 Пример результата выполнения программы:
 Пример выполняемого кода:
@@ -32,12 +23,15 @@ False
 """
 
 
-def call_counter(func=None):
-    calls_ = 0
+def call_counter(func):
+    call_counter.__count = 0
 
     def wrapper(*args, **kwargs):
-        return func(*args, **kwargs) if func else calls_
+        wrapper.count += 1
+        call_counter.__count += 1
+        return func(*args, **kwargs)
 
+    wrapper.count = 0
     return wrapper
 
 
@@ -51,13 +45,19 @@ def is_contains(pattern: str, list_to_search: list[str]) -> bool:
     return True if pattern.lower() in map(lambda x: x.lower(), list_to_search) else False
 
 
+def get_calls_count() -> int | str:
+    return getattr(call_counter, '__count', 'No calls')
+
+
 def main():
     print(string_info('Capybara'))
     print(string_info('Armageddon'))
     print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
     print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
 
-    print(call_counter()())
+    print(get_calls_count())
+    print(string_info.count)
+    print(is_contains.count)
 
 
 if __name__ == '__main__':
