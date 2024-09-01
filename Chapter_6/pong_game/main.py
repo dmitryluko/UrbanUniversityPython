4bc7ffe6c8d143fcf50be6ae193ee58b6286c489
Dmitry Morozov <9984398@gmail.com>
2024-09-01 14:01:18 +0300

    --> Pong: Main window dimensions

--- Chapter_6/pong_game/main.py
+++ Chapter_6/pong_game/main.py
@@ -1,15 +1,23 @@
 import arcade
 
 from Chapter_6.pong_game.design.colors import BG_COLOR
+from Chapter_6.pong_game.design.dimensions import SCREEN_WIDTH_1200, SCREEN_HEIGHT_800
 
 
 class Game(arcade.Window):
+    SCREEN_TITLE = " -x- Pong Game -x-"
+
     def on_draw(self):
         self.clear(BG_COLOR)
 
 
 def main():
-    game = Game()
+    game_window = Game(
+        width=SCREEN_WIDTH_1200,
+        height=SCREEN_HEIGHT_800,
+        title=Game.SCREEN_TITLE
+    )
+
     arcade.run()
 
 
