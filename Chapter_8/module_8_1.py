25534d78c20fe02085d557140a5a5da4770b14a3
Dmitry Morozov <9984398@gmail.com>
2024-09-14 16:53:32 +0300

    module8_1: Done, Review ready.

---
+++ Chapter_8/module_8_1.py
@@ -0,0 +1,30 @@
+"""
+Задание "Программистам всё можно":
+Пора разрушать шаблоны привычного нам Python! Вот вас раздражает, что мы не можем сложить число(int) и строку(str)? Давайте исправим это недоразумение!
+
+Реализуйте следующую функцию:
+add_everything_up, будет складывать числа(int, float) и строки(str)
+
+Описание функции:
+add_everything_up(a, b) принимает a и b, которые могут быть как числами(int, float), так и строками(str).
+TypeError - когда a и b окажутся разными типами (числом и строкой), то возвращать строковое представление этих двух данных вместе (в том же порядке). Во всех остальных случаях выполнять стандартные действия.
+
+"""
+from typing import Union
+
+
+def add_everything_up(a: Union[int, float, str], b: Union[int, float, str]) -> Union[int, float, str]:
+    try:
+        return a + b
+    except TypeError:
+        return str(a) + str(b)
+
+
+def main():
+    print(add_everything_up(123.456, 'строка'))
+    print(add_everything_up('яблоко', 4215))
+    print(add_everything_up(123.456, 7))
+
+
+if __name__ == '__main__':
+    main()
