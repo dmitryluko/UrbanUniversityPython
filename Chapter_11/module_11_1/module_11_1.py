d2250ab569aef64e60bcfe757bffc8ed65d77f9d
Dmitry Morozov <9984398@gmail.com>
2024-09-25 20:37:32 +0300

    --> Change API to OW

--- Chapter_11/module_11_1/module_11_1.py
+++ Chapter_11/module_11_1/module_11_1.py
@@ -1,50 +1,57 @@
 from pprint import pprint
 import requests
-from typing import Dict, Any
+from typing import Dict, Any, Tuple
 
 
 class Location:
-    BASE_URL = "https://api.tomorrow.io/v4/locations?"
+    GEOCODING_API_URL = "http://api.openweathermap.org/geo/1.0/direct"
+    ONE_CALL_API_URL = "https://api.openweathermap.org/data/3.0/onecall"
 
     def __init__(self, **kwargs):
         self.api_key = kwargs.get('api_key', None)
+        self.__coordinates = {}
+        self.__raw_data = {}
         try:
-            self.__raw_data = self._validate_and_get_location_details(**kwargs)
+            self.__coordinates = self._validate_and_get_location_details(**kwargs)
+            self.__raw_data = self._get_weather_details(**self.__coordinates)
         except ValueError as e:
             print(e)
             self.__raw_data = {}
 
-    def _validate_and_get_location_details(self, **kwargs) -> Dict[str, Any]:
+    def _validate_and_get_location_details(self, **kwargs) -> Dict[str, float]:
         if 'name' in kwargs:
             return self._find_by_name(kwargs['name'])
         elif 'latitude' in kwargs and 'longitude' in kwargs:
-            return self._find_by_coordinates(kwargs['latitude'], kwargs['longitude'])
+            return {'lat': kwargs['latitude'], 'lon': kwargs['longitude']}
         else:
             raise ValueError("Either 'name' or 'latitude' and 'longitude' must be provided")
 
-    def _find_by_name(self, _name_to_search: str) -> Dict[str, Any]:
+    def _find_by_name(self, city_name: str) -> Dict[str, float]:
         params = {
-            'name': _name_to_search,
-            'apikey': self.api_key
+            'q': city_name,
+            'appid': self.api_key
         }
-        response = requests.get(self.BASE_URL + '/', params=params)
+        response = requests.get(self.GEOCODING_API_URL, params=params)
         response.raise_for_status()
-        return response.json()
+        results = response.json()
+        if not results:
+            raise ValueError("City not found")
+        return {'lat': results[0]['lat'], 'lon': results[0]['lon']}
 
-    def _find_by_coordinates(self, _lat: float, _lon: float) -> Dict[str, Any]:
+    def _get_weather_details(self, **coordinates) -> Dict[str, Any]:
         params = {
-            'lat': _lat,
-            'lon': _lon,
-            'apikey': self.api_key
+            'lat': coordinates['lat'],
+            'lon': coordinates['lon'],
+            'appid': self.api_key
         }
-        response = requests.get(self.BASE_URL, params=params)
+        response = requests.get(self.ONE_CALL_API_URL, params=params)
         response.raise_for_status()
         return response.json()
 
 
 # Example usage
 def main():
-    api_key = '2BvHDotVS5UZNsYGl6NuNvOZPkOnyvPo'
+    api_key = '088762ed0e87d25d2afadf68da481fe2'
     location = Location(api_key=api_key, name='Moscow')
     pprint(location.__dict__)
 
