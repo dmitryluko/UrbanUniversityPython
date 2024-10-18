222423a72312cccd7d4b0da069fc198583b88a6b
Dmitry Morozov <9984398@gmail.com>
2024-10-18 12:50:08 +0300

    --> tests_12_4 : Implemented. Refactoring ->

--- Chapter_12/tests/tests_12_4.py
+++ Chapter_12/tests/tests_12_4.py
@@ -2,6 +2,8 @@ import json
 import logging.config
 import pathlib
 import unittest
+
+from functools import wraps
 from distutils.command.config import config
 
 from Chapter_12.rt_with_exceptions import Runner
@@ -22,44 +24,56 @@ class RunnerTest(unittest.TestCase):
 
     def setUp(self):
         setup_logging()
-        logger.debug('Starting "setUp"')
         self.is_frozen = False
+        logger.info('-' * 50)
 
     @staticmethod
     def _skip_if_frozen(func):
         # Assuming the skipping logic based on 'is_frozen'
+        @wraps(func)
         def wrapper(self, *args, **kwargs):
             if getattr(self, 'is_frozen', False):
                 self.skipTest("Skipping because the instance is frozen")
+                logger.warning('Test is frozen!')
             else:
                 return func(self, *args, **kwargs)
 
         return wrapper
 
     @_skip_if_frozen
-    def test_walk_to_raise(self):
-        logger.info('Starting "test_walk_to_raise" with Exception : ')
+    def test_runner_to_raise_value_err(self):
         with self.assertRaises(ValueError) as cm:
             _ = Runner(name="John Doe", speed=-5)
 
-        logger.info(f'Test to wrong args for walk() produces : {cm.exception}')
+        logger.info(f'Test to wrong args VALUE for Runner() produces : {cm.exception}')
 
         self.assertTrue(isinstance(cm.exception, ValueError))
         self.assertEqual(cm.exception.args[0], 'Скорость не может быть отрицательной, сейчас -5')
+
+        logger.info(f'{self.test_runner_to_raise_value_err.__name__}() : PASSED')
 
     @_skip_if_frozen
     def test_run(self):
         runner = Runner("TestRunner")
         for _ in range(10):
             runner.run()
+
         self.assertEqual(runner.distance, 100)
 
-    @_skip_if_frozen
-    def test_run_to_rise(self):
-        logger.info('Starting "test_run with Exception : "')
+        logger.info(f'{self.test_run.__name__}(): PASSED')
 
-        with self.assertRaises(ValueError):
-            _ = Runner(name="John Doe", speed=-5)
+    @_skip_if_frozen
+    def test_runner_to_rise_type_err(self):
+
+        with self.assertRaises(TypeError) as cm:
+            _ = Runner(name=12345, speed=10)
+
+        logger.info(f'Test to wrong args TYPE for Runner() produces : {cm.exception}')
+
+        self.assertTrue(isinstance(cm.exception, TypeError))
+        self.assertEqual(cm.exception.args[0], 'Имя может быть только строкой, передано int')
+
+        logger.info(f'{self.test_runner_to_rise_type_err.__name__}() : PASSED')
 
     @_skip_if_frozen
     def test_challenge(self):
@@ -70,10 +84,13 @@ class RunnerTest(unittest.TestCase):
                 runner1.run()
                 runner2.walk()
             self.assertNotEqual(runner1.distance, runner2.distance)
+
             logger.info('"test_challenge" выполнен успешно')
         except Exception as e:
             logger.exception("Exception in test_challenge")
             self.fail("Exception was raised: " + str(e))
+
+        logger.info(f'{self.test_challenge.__name__}() : PASSED')
 
 
 if __name__ == '__main__':
